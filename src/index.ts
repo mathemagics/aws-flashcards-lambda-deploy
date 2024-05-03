3871be62502df1a40e1bf65b8c06d34692d82d31
Paul Bodnar <higgsbison@gmail.com>
2024-05-02 22:46:21 -0600

    config

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 import { LambdaClient, UpdateFunctionCodeCommand } from "@aws-sdk/client-lambda";
 import { Handler } from 'aws-lambda';
 
-const region = process.env.BUCKET_REGION;
 const functionName = process.env.FUNCTION_NAME;
 
 const lambdaClient = new LambdaClient({ region: "your-region" });
 
-export const handler: Handler = async (event, context) => {
-    // Get the object from the event and show its content type
-    const s3Event = event.Records[0].Sns.Message;
-    const s3EventJson = JSON.parse(s3Event);
-    const bucket = s3EventJson.Records[0].s3.bucket.name;
-    const key = decodeURIComponent(s3EventJson.Records[0].s3.object.key.replace(/\+/g, ' '));
+export const handler: Handler = async (event) => {
+    const bucket = event.Records[0].s3.bucket.name;
+    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+
+    console.log(`Updating lambda function ${functionName} from bucket ${bucket} with key ${key}`);
 
     try {
         const updateCodeCommand = new UpdateFunctionCodeCommand({
